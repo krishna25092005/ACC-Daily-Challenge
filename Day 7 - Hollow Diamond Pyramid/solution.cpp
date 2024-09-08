7343ab54d8c3e8a1c6fcf09847c2b9cd292321c7
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-09-08 20:54:49 +0530

    Day 7 - Hollow Diamond Pyramid

--- Day 7 - Hollow Diamond Pyramid/solution.cpp
+++ Day 7 - Hollow Diamond Pyramid/solution.cpp
@@ -1,48 +1,68 @@
 #include <iostream>
 using namespace std;
 
+// Function to print the hollow diamond pattern
 void hollowDiamondPattern(int n)
 {
-    for (int i = n / 2; i >= 1; i--)
+    // First half of the diamond (upper half)
+    for (int i = n / 2; i >= 1; i--)  // Loop from n/2 down to 1
     {
-        for (int j = 1; j <= i; j++)
+        // Print the left half of stars
+        for (int j = 1; j <= i; j++)  
         {
             cout << "*";
         }
+
+        // Print the middle spaces
         for (int j = 1; j <= n - 2 * i; j++)
         {
             cout << " ";
         }
+
+        // Print the right half of stars
         for (int j = 1; j <= i; j++)
         {
             cout << "*";
         }
+        
+        // Move to the next line after each row
         cout << endl;
     }
 
-    for (int i = 1; i <= n / 2; i++)
+    // Second half of the diamond (lower half)
+    for (int i = 1; i <= n / 2; i++)  // Loop from 1 to n/2
     {
+        // Print the left half of stars
         for (int j = 1; j <= i; j++)
         {
             cout << "*";
         }
+
+        // Print the middle spaces
         for (int j = 1; j <= n - 2 * i; j++)
         {
             cout << " ";
         }
+
+        // Print the right half of stars
         for (int j = 1; j <= i; j++)
         {
             cout << "*";
         }
+
+        // Move to the next line after each row
         cout << endl;
     }
 }
-int main()
 
+int main()
 {
     int n;
-    cout << "Enter n: ";
-    cin >> n;
+    cout << "Enter n: ";  // Input the value of n
+    cin >> n;  // Read the value of n from the user
+
+    // Call the function to print the hollow diamond pattern
     hollowDiamondPattern(n);
-    return 0;
+
+    return 0;  // End of the program
 }
